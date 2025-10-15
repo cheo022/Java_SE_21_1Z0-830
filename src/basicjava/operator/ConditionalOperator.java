1404c3a59c3d4fe8082c762f64574c4544326243
cheo022 <jimenezvillajoseluis@gmail.com>
2025-10-15 14:13:25 -0500

    operators and exercises

---
+++ src/basicjava/operator/ConditionalOperator.java
@@ -0,0 +1,22 @@
+package basicjava.operator;
+
+public class ConditionalOperator {
+    public static void main(String[] args) {
+
+        int number1 = 5, number2 = 8, number3 = 12;
+        boolean result; //default value is false
+
+        // Conditional AND (&&) operator
+        result = (number1 > number2) && (number3 > number1);
+        System.out.println("(number1 > number2) && (number3 > number1) --> result : " + result);
+
+        // Conditional OR (||) operator
+        result = (number1 > number2) || (number3 > number2);
+        System.out.println("(number1 > number2) || (number3 > number2) --> result : " + result);
+
+        // Conditional Ternary (?:) operator
+        result = number1 < number2 ? true : false;
+        System.out.println("number1 < number2 ? true : false --> result : " + result);
+
+    }
+}
--- src/basicjava/operator/EqualityRelationalOperator.java
+++ src/basicjava/operator/EqualityRelationalOperator.java
@@ -3,7 +3,8 @@ package basicjava.operator;
 public class EqualityRelationalOperator {
     public static void main(String[] args) {
 
-        int a = 5, b = 5;
+//        int a = 5, b = 5;
+        int a = 2, b = 5;
 
         System.out.println("a == b : " + (a == b));
         System.out.println("a != b : " + (a != b));
